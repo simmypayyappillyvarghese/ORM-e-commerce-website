990ac2c910ee991f613c262f14dbde5506e06044
simmy varghese <simmyvarghese5@gmail.com>
2022-07-06 14:46:28 -0700

    Added the get routes and categories update route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,22 +3,73 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  try{
+  const categoryData=await Category.findAll(
+
+    {include:[{model:Product}]}
+  );
+
+  if(!categoryData){
+    return res.status(404).json({message:"No data found"})
+  }
+  return res.status(200).json(categoryData);
+  }
+  catch(e){
+    console.log(e);
+  }
+
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async(req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try{
+  const categoryData=await Category.findByPk(req.params.id,
+    {include:[{model:Product}]}
+  );
+
+  if(!categoryData){
+    return res.status(404).json({message:"No data found"});
+  }
+  return res.status(200).json(categoryData);
+
+  }
+  catch(e){console.log(e)}
 });
 
-router.post('/', (req, res) => {
+router.post('/', async(req, res) => {
   // create a new category
+  try{
+  const categoryData=await Category.create(req.body);
+
+  if(!categoryData){
+    return res.status(404).json({message:"Failed To create the data"});
+  }
+  return res.status(200).json(categoryData);
+}
+catch(e){console.log(e)}
 });
 
-router.put('/:id', (req, res) => {
+
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try{
+    const categoryData=await Category.update(req.body,{
+      where:{
+        id:req.params.id
+      }
+    });
+  
+    if(!categoryData){
+      return res.status(404).json({message:"Failed To update the data"});
+    }
+    return res.status(200).json(categoryData);
+  }
+  catch(e){console.log(e)}
+
 });
 
 router.delete('/:id', (req, res) => {
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,16 +4,40 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/',async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+ try{
+  const productData=await Product.findAll({
+
+    include:[{model:Category},{model:Tag,through:ProductTag,as:'product_with_tag'}]
+  });
+
+  if(!productData){
+    return res.status(404).json({message:"No product Data Found"});
+  }
+  return res.status(200).json(productData);
+}
+catch(e){console.log(e)}
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async(req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+
+  try{
+  const productData=await Product.findByPk(req.params.id,{
+    include:[{model:Category},{model:Tag,through:ProductTag,as:'product_with_tag'}]
+  })
+  if(!productData){
+    return res.status(404).json({message:"No product Data Found"});
+  }
+  return res.status(200).json(productData);
+}
+catch(e){console.log(e)}
 });
+
 
 // create new product
 router.post('/', (req, res) => {
